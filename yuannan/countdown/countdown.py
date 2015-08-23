5858e1097aa3451c675ddf0999e9a59defbd33a6
Yuannan <yuannan@live.com>
2015-08-23 16:36:52 +0100

    fixed a bug that stoped it from working

--- yuannan/countdown/countdown.py
+++ yuannan/countdown/countdown.py
@@ -1,10 +1,19 @@
+#/usr/bin/env python
+#
+#A quick and simple script that output a "run and replace" timer
+#Can be change to anything, but currently support it in minutes max
+#Easily modded to create a a hours option
+
 import time
+from time import sleep
 
 def countdown(t):
-    while time:
+    while t > 0:
         mins, secs = divmod(t, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print(timeformat, end='\r')
-        time.sleep(1)
-        time -= 1
+        sleep (1)
+        t -= 1
     print('Times Up!!!')
+
+
