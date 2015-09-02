d438d64f7181c9f3f480ac2437a6acf9afa7741a
Yuannan Lin <yuannan@users.noreply.github.com>
2015-09-02 16:10:39 +0100

    Delete starttest.py

--- yuannan/pizzashop/starttest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import csv
-
-
-submenus=["starters", "pizzas", "drinks", "desserts", "extras"]
-open("etc/"+submenus[0]+".csv")
-print(csv.reader(open("etc/"+submenus[0]+".csv")))
